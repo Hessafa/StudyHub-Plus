624e90bfff37722bf191c285b588aa481d6ce062
KhuloodGhazi <khulood.gmo@gmail.com>
2025-10-10 14:51:25 +0300

    Fix app react instllation

---
+++ StudyHub/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+// For SVG files
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+// For CSS files
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
