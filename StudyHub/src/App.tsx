0e9bc13758eeca0c520f346260994a0d42a34612
Hussah ✨ <10685@holbertonstudents.com>
2025-10-17 15:12:22 +0300

    Add timer route to App component

--- StudyHub/src/App.tsx
+++ StudyHub/src/App.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login"; 
 import Register from "./pages/Register"; 
-
+import timer from "./timer";
+import './styles.css';  
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} /> 
+       <Route path="/timer" element={<timer />} />
     </Routes>
   );
 }
