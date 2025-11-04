f96f5faadf5e6cbd66b7e418ae988ddb881cfdcb
Hussah ✨ <10685@holbertonstudents.com>
2025-11-04 12:38:53 +0300

    Implement survey routes in App component
    Added survey functionality with new routes for SurveyForm and Result components.

--- StudyHub/src/App.tsx
+++ StudyHub/src/App.tsx
@@ -1,15 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from "./pages/Login"; 
-import Register from "./pages/Register"; 
-import timer from "./timer";
-import './timer.css';  
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Existing pages
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Timer from "./timer"; // Capitalized: React components must start with uppercase
+
+// Survey components
+import SurveyForm from "./components/SurveyForm";
+import Result from "./components/Result";
+
+import "./App.css";
+import "./timer.css";
 
 export default function App() {
+  const [result, setResult] = useState<any>(null);
+
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} /> 
-       <Route path="/timer" element={<timer />} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        {/*  Existing routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/timer" element={<Timer />} />
+
+        {/*  New survey route */}
+        <Route
+          path="/survey"
+          element={
+            !result ? (
+              <SurveyForm setResult={setResult} />
+            ) : (
+              <Result result={result} setResult={setResult} />
+            )
+          }
+        />
+      </Routes>
+    </div>
   );
 }
